01b76a08512fb06599793bc9ea0d1e8c3500ad3a
LuisF <luis.freitez94@gmail.com>
2017-02-13 11:05:46 -0400

    repaldo

--- redsocial/serializers.py
+++ redsocial/serializers.py
@@ -1,9 +1,30 @@
 
 from django.core.urlresolvers import reverse
 from redsocial.models import Post, Comment, Resource, User, Profile, Area_Conocimiento,\
-                                 Canal, Like_Post
+                                 Canal, Like_Post, Interes
 from rest_framework import routers, serializers, viewsets
 from rest_framework.relations import SlugRelatedField
+
+#
+# *****************************PROFILE SERIALIZER'S**************************************
+#
+class Area_ConocimientoSeriaizer(serializers.ModelSerializer):
+    class Meta:
+        model = Area_Conocimiento
+        fields =('id', 'nombre', 'descripcion' )
+
+class InteresesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Interes
+        fields = ('interes')
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    #  intereses = InteresesSerializer()
+    class Meta:
+        model = Profile
+        fields = ('owner', 'nombre', 'apellido', 'telefono', 'direccion',\
+                  'fecha_nacimiento', 'conocimientos')
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,6 +33,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'name', 'avatar')
         ordering = ('-created',)
 
+#
+# *****************************POST SERIALIZER'S**************************************
+#
 
 class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,17 +43,16 @@ class OwnerSerializer(serializers.ModelSerializer):
         fields = ('username', 'name', 'avatar')
 
 class CommentSerializer(serializers.ModelSerializer):
-    #owner = OwnerSerializer(many=True)
     class Meta:
         model = Comment
-        fields = ('id', 'created', 'content', 'url', 'post', 'user')
+        fields = ('created', 'content', 'url', 'post', 'owner')
         ordering = ('-created',)
 
-        def user(self, instance):
-            return reverse('user', kwargs={'username':instance.username})
-        #
+       # def user(self, instance):
+       #    return reverse('user', kwargs={'username':instance.username})
+       #
        # def post(self, instance):
-        #    return reverse('post', kwargs={'id':instance.post.id})
+       #    return reverse('post', kwargs={'id':instance.post.id})
 
 class LikesSerializer(serializers.ModelSerializer):   
     class Meta:
@@ -37,35 +60,38 @@ class LikesSerializer(serializers.ModelSerializer):
         fields = ('post', 'owner')
 
 
-
-class ResourceSerializer(serializers.ModelSerializer):
+class ResourceSerializer(serializers.ModelSerializer):    
     class Meta:
-        model = Resource
-        fields = ('id', 'created', 'content', 'url', 'post', 'owner')
+        model = Resource    
+        fields = ('created', 'content', 'url', 'post', 'content_type')    
         ordering = ('-created',)
+
  
+class PostSerializer(serializers.ModelSerializer):
+    comments = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='comments'
+    )    
+    resources = ResourceSerializer(many=False)
+    class Meta:
+        model  = Post
+        fields = ('created', 'content', 'status', 'url', 'resources',\
+                 'owner', 'likespost', 'comments')
+        ordering = ('-created',)
+
 
 class TimelineSerializer(serializers.ModelSerializer):
-    owner = OwnerSerializer()
-    comment = serializers.RelatedField(many=True, read_only='True')
-    comment = CommentSerializer(many=True)
+    posts = PostSerializer()
     class Meta:
-        model = Post
-        fields = ('id', 'created', 'content', 'url', 'owner', 'comment', 'likes', 'resources')
-        ordering = ('-created',)
+        #model = Post
+        fields = ('posts')
+        #fields = ('id', 'created', 'content', 'url', 'owner', 'comment', 'likes', 'resources')
+        #ordering = ('-created',)
 
         #def owner(self, instance):
          #   return reverse('user', kwargs={'username':instance.username})
 
-class PostSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(many=True, read_only=True)
-    resources = ResourceSerializer(many=True, read_only = True)
-    likes = LikesSerializer(many=True, read_only=True, source='like')
-
-    class Meta:
-        model  = Post
-        fields = ('id', 'created', 'content', 'status', 'url', 'owner', 'comments', 'like', 'resources')
-        ordering = ('-created',)
 
 
 #class PostSerializer(serializers.HyperlinkedModelSerializer):
@@ -76,32 +102,6 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 
-# def post(self, instance):
-#                 return reverse('post', kwargs={'id':instance.post.id})
-#
-#         def user(self, instance):
-#                 return reverse('user', kwargs={'username':instance.username})
-
-
-
-#
-# *****************************PROFILE SERIALIZER'S**************************************
-#
-class Area_ConocimientoSeriaizer(serializers.ModelSerializer):
-    class Meta:
-        model = Area_Conocimiento
-        fields =('id', 'nombre', 'descripcion' )
-
-
-class ProfileSerializer(serializers.ModelSerializer):
-    #conocimiento = Area_ConocimientoSeriaizer(many=True, read_only=True, source='profile')
-    #conocimientos = serializers.RelatedField(many=True, read_only=True)
-    #intereses = serializers.ManyRelatedField(many=True)
-    class Meta:
-        model = Profile
-        fields = ('owner', 'nombre', 'apellido', 'telefono', 'direccion',\
-                  'fecha_nacimiento', 'conocimientos', 'intereses')
-
 
 #
 # **********************CANAL SERIALIZER'S*********************************************
@@ -111,13 +111,20 @@ class PostCanalSerializer(serializers.ModelSerializer):
     post = PostSerializer()
 
 
-class CanalSerializer(serializers.ModelSerializer):
+"""class CanalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Canal 
         fields = ('id', 'nombre', 'descripcion', 'created', 'owner', 'miembro', 'postcanal')
-    """ miembros = serializers.SlugRelatedField(
+     miembros = serializers.SlugRelatedField(
         many=True,
         read_only=True,
         slug_field='miembro'
     )"""
 
+class CanalSerializer(serializers.ModelSerializer):
+    
+    postcanal = PostSerializer(many=True, read_only=True, source='set_post')
+    
+    class Meta:
+        model = Canal
+        fields = ('id', 'nombre', 'descripcion','created', 'owner', 'miembro', 'areaconocimientos', 'postcanal')
--- ucline/urls.py
+++ ucline/urls.py
@@ -13,36 +13,56 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import patterns as patterns
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from rest_framework.urlpatterns import format_suffix_patterns 
+
 from rest_framework import routers
 
 from redsocial import views
 from redsocial.views import *
 
-
 comment_creation = views.PostViewSet.as_view({
     'post': 'set_comment'
 })
 
-profile_retrive = views.ProfileViewsSet.as_view({
+#
+#****************METHODS CANAL*************************
+#
+canal_list = views.CanalViewsSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+canal_detail = views.CanalViewsSet.as_view({
     'get': 'retrieve',
     'put': 'update',
     'patch': 'partial_update',
     'delete': 'destroy'
 })
 
+postcanal_creation = views.CanalViewsSet.as_view({
+    'post': 'set_post'
+})
+
+#
+#******************************************************
+#
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
-router.register(r'timeline', TimelineViewSet)
-router.register(r'post', PostViewSet)
-#router.register(r'post/(?P<pk>[0-9]+)/comment/', comment_creation)
-router.register(r'comment', CommentViewSet)
-router.register(r'resource', ResourceViewSet)
 router.register(r'profile', ProfileViewsSet)
 router.register(r'area_conocimiento', Area_ConocimientoViewsSet)
-router.register(r'canal', CanalViewsSet)
+
+router.register(r'timeline', TimelineViewSet)
+
+router.register(r'post', PostViewSet)
+router.register(r'comment', CommentViewSet)
+router.register(r'like', LikesViewSet)
+router.register(r'resource', ResourceViewSet)
+
+#router.register(r'canal', CanalViewsSet)
 
 
 
@@ -52,5 +72,11 @@ urlpatterns = [
     url(r'^entities/', include(router.urls)),
     url(r'^post/(?P<pk>[0-9]+)/comment/', comment_creation),
     url(r'^', include('redsocial.urls')),
+    #-----Canal urls-------------------------
+    url(r'^entities/canal/', canal_list, name='canal_list'), 
+    url(r'^entities/canal/(?P<pk>[0-9]+)/', canal_detail, name='canal_detail'), 
+    url(r'^entities/canal/(?P<pk>[0-9]+)/post/', postcanal_creation, name='postcanal_creation'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
